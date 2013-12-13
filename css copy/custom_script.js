2ee78323bbb6dde15b421cb9828d8df82b86d1ca
Kumar Rachuri <KumarR28@me.com>
2013-12-12 19:57:53 -0500

    Contact page update
    update to contact page, CSS and JS

--- css copy/custom_script.js
+++ css copy/custom_script.js
@@ -7,9 +7,9 @@
     }
 	
 	//CUSTOM JS EFFECTS FOR TESTIMONIALS
-    function showKumarBody(inId){
-       // alert("TEST");
-        $(".kumar-body").fadeOut("slow");
-        $("#"+inId+">.kumar-body").slideDown("slow");
-        		   	//$("div#testimonial-body").slideDown("slow");
-    }
+    //function showKumarBody(inId){
+        //alert("TEST");
+      //  $(".kumar-body").fadeOut("slow");
+        //$("#"+inId+">.kumar-body").slideDown("slow");
+        //$("div#testimonial-body").slideDown("slow");
+    //}
